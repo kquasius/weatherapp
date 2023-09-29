f955e8cf562eefa846c250eeb2f73acb4e020913
kquasius <146397345+kquasius@users.noreply.github.com>
2023-09-28 19:49:43 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,15 +1,26 @@
 import streamlit as st
-import webbrowser
+import requests
 
 st.set_page_config(page_title="Katie's Weather App")
 st.header("Enter your location to begin!")
 
 def home_page():
-  location = st.text_input('Your Location')
-  if location != '':
-    getLocation(location)
+  city = st.text_input('City')
+  state = st.selectbox('State', {'WI', 'IL'})
+  st.button('Submit')
+  if submit:
+    getLonLat(city, state)
 
-def getLocation(loc):
-  st.write(loc)
+def getLonLat(city, state):
+  API_KEY = 'eee3e1e17b0471238be18e8f6cfbfee6'
+  geo_url = 'http://api.openweathermap.org/geo/1.0/direct?q={'+ city + state + 'US&limit={limit}&appid=' + API_KEY
+  geo_params {
+    "lat": lat,
+    "lon": lon }
+  geo = requests.get(geo_url, geo_params)
+  st.write(geo)
+
+def getLocation(lon, lat):
+  st.write()
 
 home_page()
