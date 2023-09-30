dae4e223a3204657c17941e6f7dffbd2d092042e
kquasius <146397345+kquasius@users.noreply.github.com>
2023-09-30 11:22:57 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -10,13 +10,10 @@ def home_page():
 
 def getWeather():
   API_KEY = 'eee3e1e17b0471238be18e8f6cfbfee6'
-  base_url = https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-  params = {
-    lat = 41.881832
-    lon = -87.623177
-    API_key = API_KEY
-}
-  response = requests.get(base_url, params)
+  lat = 41.881832
+  lon = -87.623177
+  url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_KEY}'
+  response = requests.get(url)
   weather = response.json()
   st.write(weather)
 
