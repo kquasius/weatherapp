19f898ee9812fcbf29fa36c5ba50fb7aaba0fc55
kquasius <146397345+kquasius@users.noreply.github.com>
2023-09-30 11:34:37 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -5,22 +5,40 @@ st.set_page_config(page_title="Katie's Weather App")
 st.header("Enter your location to begin!")
 
 def home_page():
-  st.write("The current weather for Chicago, IL is:")
-  getWeather()
+  city = st.text_input()
+  temp = getWeather(city)
+  if temp > 74:
+    sunny_and_hot()
+  elif temp > 65:
+    sunny_and_warm()
 
-def getWeather():
+def getWeather(city):
   api_key = 'eee3e1e17b0471238be18e8f6cfbfee6'
-  city = 'Chicago'
   url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
   response = requests.get(url)
   if response.status_code == 200:
     data = response.json()
-    temp = data['main']['temp']
+    tempK = data['main']['temp']
     desc = data['weather'][0]['description']
+    temp = (tempK − 273.15) × 1.8 + 32
     st.write("Tempurature: ", temp)
     st.write("Weather: ", desc)
   else:
     st.write("Error fetching weather data.")
+
+  return temp
+
+def sunny_and_hot():
+  st.write("It's sunny and hot!")
+  st.write("Outfit: t-shirt and shorts")
+  st.write("Album: Solar Power")
+  st.write("Activity: Go swimming!")
+
+def sunny_and_warm():
+  st.write("It's sunny and warm!")
+  st.write("Outfit: t-shirt and pants")
+  st.write("Album: Harry's House")
+  st.write("Activity: Go for a walk")
 
 
 home_page()
